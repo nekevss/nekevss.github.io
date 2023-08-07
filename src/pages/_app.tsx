867ed8860869d18866aae207c045ba7e231c9f6b
nekevss <nekevss@gmail.com>
2023-08-07 16:31:03 -0400

    Adding ai favicon, cause I can

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Layout from '@/layout/SiteLayout'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
