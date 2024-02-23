22ca3c2e8ab643d4b5eec2167ea61ede9c192650
nekevss <nekevss@gmail.com>
2024-02-22 19:09:36 -0500

    Add a touch more mobile responsiveness to blog

--- src/components/MarkdownDisplay.tsx
+++ src/components/MarkdownDisplay.tsx
@@ -15,14 +15,14 @@ export default function MarkdownDiplay(props: MarkdownDisplayProps) {
         ["h2", "class", "text-xl font-bold text-center my-8"],
         ["h3", "class", "text-lg font-bold text-center my-8"],
         ["p", "class", "my-6"],
-        ["code", "class", "bg-theme rounded-sm font-mono px-1",],
-        ["pre", "class", "w-full whitespace-pre-wrap bg-theme rounded-lg font-mono py-1 px-3"]
+        ["code", "class", "bg-theme overflow-x-auto rounded-sm font-mono px-1",],
+        ["pre", "class", "w-full whitespace-pre overflow-x-auto lg:whitespace-pre-wrap bg-theme rounded-lg font-mono py-1 px-3"]
     ])
 
     return (
         <div
             dangerouslySetInnerHTML={{__html: content}}
-            className="min-h-min w-4/6 lg:w-2/5 mx-auto my-8 justify-items-between">
+            className="min-h-min w-5/6 lg:w-2/5 mx-auto my-8 justify-items-between">
         </div>
     )
 }
